368f418ef82fe1ecf2b2d8723ea4923dd1d34e6a
ZsZs <zsolt.zsuffa@gmail.com>
2018-03-23 18:29:41 +0100

    refine build

--- processpuzzle-microservice-seed-ui/src/components/tick-tock/tick-tock.component.spec.ts
+++ processpuzzle-microservice-seed-ui/src/components/tick-tock/tick-tock.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
-import { TickTockModule } from '../../';
+import { ProcessPuzzleMicroserviceSeedUiModule } from '../../';
 import { TickTockComponent } from './tick-tock.component';
 
 describe('TickTockComponent', () => {
@@ -13,7 +13,7 @@ describe('TickTockComponent', () => {
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        imports: [ TickTockModule ]
+        imports: [ ProcessPuzzleMicroserviceSeedUiModule ]
       }).compileComponents().then(() => { /* Don't do anything */ });
     })
   );
